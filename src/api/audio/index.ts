2bf59f9bc124e9acf62c9a70a4ef7fed9ef2b451
Rein van Haaren <rein@vanhaaren.nl>
2024-12-17 22:26:29 +0100

    feat: audio pots api definitions added

--- src/api/audio/index.ts
+++ src/api/audio/index.ts
@@ -2,6 +2,7 @@ import type { DHDGetHandlers, DHDSetHandlers } from '@/types';
 
 import { audioLevelsGetHandlers } from './levels';
 import { audioLoudnessGetHandlers } from './loudness';
+import { audioPotsGetHandlers, audioPotsSetHandlers } from './pots';
 import { audioRoutingGetHandlers, audioRoutingSetHandlers } from './routing';
 import {
   audioSelectorsGetHandlers,
@@ -11,11 +12,13 @@ import {
 export const audioGetHandlers = {
   ...audioLevelsGetHandlers,
   ...audioLoudnessGetHandlers,
+  ...audioPotsGetHandlers,
   ...audioRoutingGetHandlers,
   ...audioSelectorsGetHandlers,
 } satisfies DHDGetHandlers;
 
 export const audioSetHandlers = {
+  ...audioPotsSetHandlers,
   ...audioRoutingSetHandlers,
   ...audioSelectorsSetHandlers,
 } satisfies DHDSetHandlers;
---
+++ src/api/audio/pots/index.ts
@@ -0,0 +1,41 @@
+import { z } from 'zod';
+
+import type { DHDGetHandlers, DHDSetHandlers } from '@/types';
+
+import { numberedObject } from '@/utils';
+
+const audioPotsPotImmutable = z.object({
+  _max: z.number(),
+  _min: z.number(),
+  _name: z.string(),
+  _step: z.number(),
+});
+
+const audioPotsPotMutable = z.object({
+  value: z.number(),
+});
+
+const audioPotsPot = audioPotsPotImmutable.merge(audioPotsPotMutable);
+
+export const audioPotsGetHandlers = {
+  ['/audio/pots']: {
+    paramsSchema: null,
+    responseSchema: numberedObject(audioPotsPot),
+  },
+  ['/audio/pots/{potID}']: {
+    paramsSchema: z.object({
+      potID: z.number(),
+    }),
+    responseSchema: audioPotsPot,
+  },
+} satisfies DHDGetHandlers;
+
+export const audioPotsSetHandlers = {
+  ['/audio/pots/{potID}']: {
+    paramsSchema: z.object({
+      potID: z.number(),
+    }),
+    payloadSchema: audioPotsPotMutable,
+    responseSchema: audioPotsPotMutable,
+  },
+} satisfies DHDSetHandlers;
